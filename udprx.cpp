5c8f473a045595e289476fec372f1ec8445a0999
dt100@naboo <dt100@naboo>
2023-12-14 17:41:22 +0000

    simple archiving option:
    -o,--output INT             1: output data to stdout
    eg
    -o 1 | pv > /dev/null
    -o 1 > BGIRAWFILE
    
    -c -1 default: no validation .. validation is good, but if it's not
    possible it's bad.
     On branch pgmwashere
    	modified:   udprx.cpp

--- udprx.cpp
+++ udprx.cpp
@@ -8,6 +8,7 @@
 #include <common/Socket.h>
 #include <common/Timer.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "rt.h"
 
@@ -18,8 +19,10 @@ struct {
   int SocketBufferSize{2000000};
   int SamplesPerPacket{1};
   int SampleSizeBytes{64};
-  int CountColumn{1};
-  int RtPrio;
+  int CountColumn{-1};
+  int RtPrio{0};
+  int outfd{-1};
+  int quiet{0};
 } Settings;
   
 void fmtElapsedTime(char *str, int tick, int tock) {
@@ -45,6 +48,8 @@ int main(int argc, char *argv[]) {
   app.add_option("--ssb", Settings.SampleSizeBytes, "Sample size (bytes)");
   app.add_option("-c, --count_column", Settings.CountColumn, "Count column (indexed from 0)");
   app.add_option("-R, --rt_prio", Settings.RtPrio, "set POSIX RT priority (0: no set)");
+  app.add_option("-o, --output", Settings.outfd, "1: output data to stdout");
+  app.add_option("-q, --quiet", Settings.quiet, "1: stop reporting");
   CLI11_PARSE(app, argc, argv);
 
   static const int BUFFERSIZE{9200};
@@ -82,54 +87,63 @@ int main(int argc, char *argv[]) {
     int ReadSize = Receive.receive(buffer, BUFFERSIZE);
 
     assert(ReadSize > 0);
-    //printf("ReadSize = %d\n",ReadSize);fflush(stdout);
     assert(ReadSize == Settings.DataSize);
 
-    if (ReadSize > 0) {
-      SeqNo = *((uint32_t *)buffer);
-      RxBytes += ReadSize;
-      RxPackets++;
-    }
-
-    // Grab the first SPAD Count in case we're not starting from 1
-    if (RxPackets == 1) {
+    SeqNo = *((uint32_t *)buffer);
+    RxBytes += ReadSize;
+    RxPackets++;
+    if (RxPackets == 1){
         tick = time(0);
-        SpadTracker = *((uint32_t *)( buffer + 4*Settings.CountColumn ));
     }
 
-    for (int i = 0; i <= Settings.SamplesPerPacket-1; i++) {
-        SpadIndex = ( i*Settings.SampleSizeBytes+4*Settings.CountColumn );
-
-        SpadCount = *((uint32_t *)( buffer + SpadIndex ));
-        if (RxPackets <= 5 || deviation == true) {
-            printf("%#010x    %i\n",SpadCount,SpadCount);fflush(stdout);
-	    deviation = false;
+    if (Settings.CountColumn >= 0){
+        // Grab the first SPAD Count in case we're not starting from 1
+        if (RxPackets == 1) {
+            SpadTracker = *((uint32_t *)( buffer + 4*Settings.CountColumn ));
         }
-        if (SpadTracker != SpadCount) {
-	    deviation = true;
-            ErrCount = ErrCount + 1;
-            printf("Deviation! ErrCount = %i, Expected SPAD : %i, Received SPAD : %i, Completed Packets = %li, Sample Jump = %i, Packets Lost = %i, Bytes = %i\n", \
-			    ErrCount, SpadTracker, SpadCount, RxPackets-1, ((SpadCount - SpadTracker) / Settings.SamplesPerPacket), (SpadCount - SpadTracker)/Settings.SamplesPerPacket, (Settings.SampleSizeBytes * (SpadCount - SpadTracker)/Settings.SamplesPerPacket));fflush(stdout);
-            SpadTracker = SpadCount; // Ignore error, reinitialise tracker variable
-            if (ErrCount > 9) {
-                exit(0);
+
+        for (int i = 0; i <= Settings.SamplesPerPacket-1; i++) {
+            SpadIndex = ( i*Settings.SampleSizeBytes+4*Settings.CountColumn );
+
+            SpadCount = *((uint32_t *)( buffer + SpadIndex ));
+            if (RxPackets <= 5 || deviation == true) {
+                fprintf(stderr, "%#010x    %i\n",SpadCount,SpadCount);fflush(stdout);
+	        deviation = false;
             }
-        }
-        SpadTracker = SpadTracker + 1;
+            if (SpadTracker != SpadCount) {
+	        deviation = true;
+                ErrCount = ErrCount + 1;
+                fprintf(stderr, "Deviation! ErrCount = %i, Expected SPAD : %i, Received SPAD : %i," \
+			        " Completed Packets = %li, Sample Jump = %i, Packets Lost = %i, Bytes = %i\n", 
+			    ErrCount, SpadTracker, SpadCount, 
+			    RxPackets-1, ((SpadCount - SpadTracker) / Settings.SamplesPerPacket), (SpadCount - SpadTracker)/Settings.SamplesPerPacket, 
+			    (Settings.SampleSizeBytes * (SpadCount - SpadTracker)/Settings.SamplesPerPacket));
+		fflush(stdout);
+            SpadTracker = SpadCount; // Ignore error, reinitialise tracker variable
+                if (ErrCount > 9) {
+                    exit(0);
+                }
+            }
+            SpadTracker = SpadTracker + 1;
+       }
     }
-
+    if (Settings.outfd >= 0){
+        write(Settings.outfd, buffer, ReadSize);
+    }
     if ((RxPackets % 100) == 0)
       USecs = UpdateTimer.timeus();
 
-    if (USecs >= intervalUs) {
+    if (!Settings.quiet && USecs >= intervalUs) {
       RxBytesTotal += RxBytes;
       time_t tock = time(0);
       fmtElapsedTime(elapsed_str,tick,tock);
-      printf("Rx rate: %.2f Mbps, rx %" PRIu64 " MB (total: %" PRIu64 " MB), Elapsed %s, ErrCount = %i, PER %4.3e\n",
-             RxBytes * 8.0 / (USecs / 1000000.0) / B1M, RxBytes / B1M / interval_s, RxBytesTotal / B1M, elapsed_str, ErrCount, (1.0 * ErrCount / RxPackets));
+      fprintf(stderr, "Rx rate: %.2f Mbps, rx %" PRIu64 " MB (total: %" PRIu64 " MB), Elapsed %s, ErrCount = %i, PER %4.3e\n",
+             RxBytes * 8.0 / (USecs / 1000000.0) / B1M, RxBytes / B1M / interval_s, 
+	     RxBytesTotal / B1M, elapsed_str, ErrCount, (1.0 * ErrCount / RxPackets));
       RxBytes = 0;
       UpdateTimer.now();
       USecs = UpdateTimer.timeus();
     }
   }
 }
+
